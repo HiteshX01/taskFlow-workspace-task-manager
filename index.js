405a5041fc8f42fe235eb044bc706f8fe3946cb7
Hitesh <sutharhitesh977@gmail.com>
2026-07-17 17:45:29 +0530

    used env and ready to deploy this basic backend

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+//const port = 3000;
+const port = Process.env.port;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.get('/hitesh', (request, response) => {
-  response.send('hii this is hitesh')
+  response.send(<h1>Hii This is HITESH, Welcome..</h1>)
 })
 
+app.get('/leetcode', (req, res) => {
+  res.send('<a href=https://leetcode.com>leetcode</a>')
+})
 app.get('/version 1 updated', (req, res) => {
   console.log('it the version 1 updated');
 })
