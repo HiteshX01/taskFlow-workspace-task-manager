286587932f9a1307b3c85a73b5beacdb10dc277c
Hitesh <sutharhitesh977@gmail.com>
2026-07-17 14:35:43 +0530

    first simple backend

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 import express from 'express';
-
+import 'dotenv/config'
 const app = express();
-const port = 3000;
+
+const port = process.env.port;
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/hitesh', (request, response) => {
-  response.send('hii this is hitesh')
+app.get('/hitesh', (req, res) => {
+  res.send('hii this is hitesh');
 })
 
+app.get('/leetcode', (req, res) => {
+  res.send('<a href="https://leetcode.com" target="_blank">go to leetcode</a>');
+})
 app.listen(port, () => {
-  console.log(`app is listening on port ${port}`);
+  console.log(`app server is running, is listening on port ${port}`);
 });
