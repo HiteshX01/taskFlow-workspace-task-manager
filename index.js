e87f5ab66a12907d28c59c78418bc04035eddb83
Hitesh <sutharhitesh977@gmail.com>
2026-07-19 23:28:20 +0530

    used built in middleware for json text or urlencoded text parsing, created a simple form and accepted form data on backend

--- index.js
+++ index.js
@@ -4,45 +4,25 @@ const app = express();
 
 import dotenv from 'dotenv';
 dotenv.config();
-//const port = 3000;
 const port = process.env.PORT || 3000;
 
-// using a third party middleware, morgan logger
+//built in middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }))
+
+//third party middleware, morgan logger
 app.use(morgan('dev'));
 
-
-// if we want that evey request must go somewhere else first before any serving
-// this is middleware , middle ware uses funtion takes three parameters always
-// this is coustom middleware
-// let we want a coustom middleware for a specific page only
-app.get('/subscription',(req,res,next) => {
-  const a = 10;
-  const b = 11;
-  console.log(a+b);
-
-  console.log('coustom middleware, only for the subscription page');
-
-  next()
-
-},(req, res) => {
-  res.send('your subscription is active');
-})
-
+// this is middleware , middleware uses funtion takes three parameters always
 app.use((req,res,next) => {
   console.log('this is middleware');
-
-  // writing middleware logic
-  const a = 32;
-  const b = 21;
-  console.log(a+b);
-
   return next(); // serve the actual respone now
 })
 
 // to serve an html page from backend, we need view engine
 app.set('view engine', 'ejs');
 
-app.get('/html', (req,res) => {
+app.get('/form', (req,res) => {
   res.render('html');
 })
 
@@ -63,6 +43,11 @@ app.get('/profile', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.send('this is about section');
+})
+
+app.post('/form-data', (req,res) => {
+  console.log(req.body);
+  res.send('<h2>form submitted</h2>')
 })
 
 app.listen(port, () => {
