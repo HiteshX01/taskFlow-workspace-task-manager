798e2dfbdae2b4484a9a6bb7b12769890e125487
Hitesh <sutharhitesh977@gmail.com>
2026-07-19 18:48:00 +0530

    learn view engine ejs and middleware, basics

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ dotenv.config();
 //const port = 3000;
 const port = process.env.PORT || 3000;
 
+// to serve an html page from backend, we need view engine
+app.set('view engine', 'ejs');
+
+app.get('/html', (req,res) => {
+  res.render('html');
+})
+
+// if we want that evey request must go somewhere else first before any serving
+// this is middleware
+app.use((req,res,next) => {
+  console.log('this is middleware');
+
+  // writing middleware logic
+  const a = 32;
+  const b = 21;
+  console.log(a+b);
+
+  return next(); // serve the actual respone now
+})
+
+
 app.get('/', (req, res) => {
   res.send('this is home page');
 });
